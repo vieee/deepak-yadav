409fa31afff761235a3bce736a2a4b659ec1fe6e
Deepak Yadav <deepak.yadav@spit.ac.in>
2025-02-18 13:15:30 +0530

    Build Success, Experience and Works sections re-jigged

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,9 +7,7 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -20,11 +18,6 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -39,9 +32,7 @@ export {
   github,
   menu,
   close,
-  css,
   docker,
-  figma,
   git,
   html,
   javascript,
@@ -52,10 +43,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
